c984cb60d43a800d5a3faf4aa9fa3f5f80c0f766
Maksim Kalik <maxkalik@gmail.com>
2021-04-02 18:33:43 +0300

    init with stack navigation

---
+++ .eslintrc.js
@@ -0,0 +1,22 @@
+module.exports = {
+  root: true,
+  extends: [
+    '@react-native-community',
+    'eslint:recommended',
+    'plugin:react/recommended',
+  ],
+  parser: '@typescript-eslint/parser',
+  plugins: ['@typescript-eslint', 'react', 'jsx-a11y', 'react-hooks'],
+  rules: {
+    'comma-dangle': [1, 'never'],
+    'arrow-parens': ['error', 'as-needed'],
+    'arrow-body-style': [
+      'error',
+      'as-needed',
+      {requireReturnForObjectLiteral: true},
+    ],
+    'react/prefer-es6-class': [1, 'always'],
+    'react/display-name': [0],
+    'react/prop-types': [0],
+  },
+};
