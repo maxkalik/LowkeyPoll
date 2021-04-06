72c3d79389846ea60013b6e05356d8a27b9ff792
Maksim Kalik <maxkalik@gmail.com>
2021-04-06 16:59:42 +0300

    votes counter

--- src/components/PollWidget/PollWidget.tsx
+++ src/components/PollWidget/PollWidget.tsx
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { useState } from 'react';
 import { View, Text, TouchableOpacity } from 'react-native';
 import { WidgetContainer, WidgetHeader } from '..';
 import {
@@ -8,20 +8,25 @@ import {
 } from './types';
 import { styles } from './styles';
 
-const PollItem: React.FC<PollItemProps> = ({ text }): JSX.Element => (
-  <TouchableOpacity style={styles.pollItemContainer}>
+const PollItem: React.FC<PollItemProps> = ({ text, onPress }): JSX.Element => (
+  <TouchableOpacity style={styles.pollItemContainer} onPress={onPress}>
     <Text style={styles.pollItemText}>{text}</Text>
   </TouchableOpacity>
 );
 
 const PollWidgetContent: React.FC<PollWidgetContentProps> = ({
   title,
-  items
+  items,
+  opPressPollItem
 }): JSX.Element => (
   <View style={styles.content}>
     <Text style={styles.pollTitle}>{title}</Text>
     {items.map(item => (
-      <PollItem key={item.text} {...item} />
+      <PollItem
+        key={item.text}
+        onPress={() => opPressPollItem(item.text)}
+        text={item.text}
+      />
     ))}
   </View>
 );
@@ -33,6 +38,12 @@ const PollWidget: React.FC<PollWidgetProps> = ({
   title,
   items
 }): JSX.Element => {
+  const [votes, setVotes] = useState(voteCounter);
+  const pollItemPress = (text: string) => {
+    console.log('voted for ', text);
+    setVotes(votes + 1);
+  };
+
   return (
     <WidgetContainer>
       <WidgetHeader
@@ -40,9 +51,13 @@ const PollWidget: React.FC<PollWidgetProps> = ({
         username={headerUsername}
         userpicSource={userpicSource}
         circleLabelTitle="votes"
-        circleLabelValue={String(voteCounter)}
+        circleLabelValue={String(votes)}
       />
-      <PollWidgetContent title={title} items={items} />
+      <PollWidgetContent
+        title={title}
+        items={items}
+        opPressPollItem={poll => pollItemPress(poll)}
+      />
     </WidgetContainer>
   );
 };
--- src/components/PollWidget/types.ts
+++ src/components/PollWidget/types.ts
@@ -1,10 +1,12 @@
 import { ImageSourcePropType } from 'react-native';
 export interface PollItemProps {
   text: string;
+  onPress: () => void;
 }
 
 export interface PollWidgetContentProps {
   title: string;
+  opPressPollItem: (value: string) => void;
   items: PollItemProps[];
 }
 
